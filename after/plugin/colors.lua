0773ae7ad75266396a7a891789dfc104816d192d
Ez-FlawLess <amirali23mohammadi2@gmail.com>
2023-04-21 14:19:29 +0330

    first commit

---
+++ after/plugin/colors.lua
@@ -0,0 +1,9 @@
+function ColorMyPencils(color)
+	color = color or "rose-pine-moon"
+
+	vim.cmd.colorscheme(color)
+
+
+end
+
+ColorMyPencils()
---
+++ lua/amirali/init.lua
@@ -0,0 +1,3 @@
+require("amirali.remap")
+require("amirali.set")
+
